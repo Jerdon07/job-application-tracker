1e0475835fea1624c450df602a6866c2d961d57f
Jerdon07 <jdlitaoen@gmail.com>
2026-05-21 23:42:13 +0800

    Refactor: Wrap the button to a link component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -14,9 +15,11 @@ export default function Home() {
             <p className="text-muted-foreground mb-10 text-xl">Capture, organize, and manage your job search in one place.</p>
 
             <div className="flex flex-col items-center gap-4">
-              <Button size={"lg"} className="h-12 px-8 text-lg font-medium">
-                Start for free <ArrowRight className="ml-2" />
-              </Button>
+              <Link href="/sign-up">
+                <Button size={"lg"} className="h-12 px-8 text-lg font-medium">
+                  Start for free <ArrowRight className="ml-2" />
+                </Button>
+              </Link>
               <p className="text-sm text-muted-foreground">
                 Free forever. No credit card required.
               </p>
