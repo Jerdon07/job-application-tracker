8fae6c2cf254b21979f37a5305a7fe22bd81efb7
Jerdon07 <jdlitaoen@gmail.com>
2026-06-09 22:16:35 +0800

    Feat: Fetch the board collection to the dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,6 @@
 import { getSession } from "@/lib/auth/auth"
+import connectDB from "@/lib/db"
+import { Board } from "@/lib/models"
 import { redirect } from "next/navigation"
 
 export default async function Dashboard() {
@@ -6,7 +8,25 @@ export default async function Dashboard() {
 
     if (!session?.user) redirect('/sign-in')
     
+    await connectDB()
+
+    const board = await Board.findOne({
+        userId: session.user.id,
+        name: 'Job Hunt',
+    })
+
+    console.log(board)
+
     return (
-        <div>Dashboard</div>
+        <div className="min-h-screen bg-white">
+            <div className="container mx-auto p-6">
+                <div className="mb-6">
+                    <h1 className="text-3xl font-bold text-black">{board.name}</h1>
+                    <p className="text-gray-600">Track you Job Applications</p>
+                </div>
+
+
+            </div>
+        </div>
     )
 }
